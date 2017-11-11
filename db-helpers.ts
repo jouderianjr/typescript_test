39790e09df4b9dbb4c3c3b36030e4d1098de6b61
Jouderian Nobre <jouderianjr@gmail.com>
2017-11-11 15:48:20 -0300

    Extract db operations to db-helpers.ts

---
+++ db-helpers.ts
@@ -0,0 +1,44 @@
+import { DBOptions, GithubUsers } from './module';
+import * as R from 'ramda';
+import * as pgPromise from 'pg-promise';
+
+const createDbConnection = () =>  {
+  // Limit the amount of debugging of SQL expressions
+  const trimLogsSize : number = 200;
+
+  // Actual database options
+  const options : DBOptions = {
+    user: 'lovely',
+    password: 'lovely',
+    host: 'localhost',
+    database: 'lovelystay_test',
+  };
+
+  console.info('Connecting to the database:',
+    `${options.user}@${options.host}:${options.port}/${options.database}`);
+
+  const pgpDefaultConfig = {
+    promiseLib: require('bluebird'),
+    // Log all querys
+    query(query) {
+      console.log('[SQL   ]', R.take(trimLogsSize,query.query));
+    },
+    // On error, please show me the SQL
+    error(err, e) {
+      if (e.query) {
+        console.error('[SQL   ]', R.take(trimLogsSize,e.query),err);
+      }
+    }
+  };
+
+  const pgp = pgPromise(pgpDefaultConfig);
+  return pgp(options);
+}
+
+const createTable = (dbConnection) : PromiseLike<any> => 
+  dbConnection.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)')
+
+const insertGithubUser = (dbConnection, user : GithubUsers) :  PromiseLike<any> =>
+  dbConnection.one( 'INSERT INTO github_users (login) VALUES ($[login]) RETURNING id', user);
+
+export { createDbConnection, createTable, insertGithubUser };
--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { DBOptions, GithubUsers } from './module';
 import * as R from 'ramda';
 import * as pgPromise from 'pg-promise';
 import githubApi from './github-api';
+import { createDbConnection, createTable, insertGithubUser } from './db-helpers';
 
 const args = process.argv;
 
@@ -12,41 +13,10 @@ if( args.length !== 3 ) {
 }
 
 const username : String = R.last(args);
+const db = createDbConnection();
 
-// Limit the amount of debugging of SQL expressions
-const trimLogsSize : number = 200;
-
-// Actual database options
-const options : DBOptions = {
-  user: 'lovely',
-  password: 'lovely',
-  host: 'localhost',
-  database: 'lovelystay_test',
-};
-
-console.info('Connecting to the database:',
-  `${options.user}@${options.host}:${options.port}/${options.database}`);
-
-const pgpDefaultConfig = {
-  promiseLib: require('bluebird'),
-  // Log all querys
-  query(query) {
-    console.log('[SQL   ]', R.take(trimLogsSize,query.query));
-  },
-  // On error, please show me the SQL
-  error(err, e) {
-    if (e.query) {
-      console.error('[SQL   ]', R.take(trimLogsSize,e.query),err);
-    }
-  }
-};
-
-const pgp = pgPromise(pgpDefaultConfig);
-const db = pgp(options);
-
-db.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)')
-.then(() => githubApi.fetchUser(username))
-.then((data: GithubUsers) => db.one(
-  'INSERT INTO github_users (login) VALUES ($[login]) RETURNING id', data)
-).then(({id}) => console.log(id))
-.then(() => process.exit(0));
+createTable(db)
+  .then(() => githubApi.fetchUser(username))
+  .then((data: GithubUsers) => insertGithubUser(db, data))
+  .then(({id}) => console.log(id))
+  .then(() => process.exit(0));
