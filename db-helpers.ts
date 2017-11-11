fb848bf6b6475b068c7cdf91b27ed0f63061647a
Jouderian Nobre <jouderianjr@gmail.com>
2017-11-11 16:09:14 -0300

    Add location field to github_users table

--- db-helpers.ts
+++ db-helpers.ts
@@ -36,9 +36,9 @@ const createDbConnection = () =>  {
 }
 
 const createTable = (dbConnection) : PromiseLike<any> => 
-  dbConnection.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)')
+  dbConnection.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, location TEXT)')
 
-const insertGithubUser = (dbConnection, user : GithubUsers) :  PromiseLike<any> =>
-  dbConnection.one( 'INSERT INTO github_users (login, name, company) VALUES ($[login], $[name], $[company]) RETURNING id', user);
+const insertGithubUser = (dbConnection, user : GithubUsers) :  PromiseLike<Number> =>
+  dbConnection.one( 'INSERT INTO github_users (login, name, company, location) VALUES ($[login], $[name], $[company], $[location]) RETURNING id', user);
 
 export { createDbConnection, createTable, insertGithubUser };
